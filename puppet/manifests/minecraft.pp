e566e8641ecf12ee7f30e481e569b8f8e12b957c
Byron <spiderslayer@reetstreet.com>
2013-11-09 20:32:24 -0800

    add ben and zoomba

--- puppet/manifests/minecraft.pp
+++ puppet/manifests/minecraft.pp
@@ -20,7 +20,7 @@ class { 'minecraft': }
 
 # Declare the operator (or operators)
 minecraft::op { "byron358": }
-
+minecraft::op { "ultimatetide": }
 # List the whitelisted players
 minecraft::whitelist { "flaryo": }
 minecraft::whitelist { "Riley_Johsnson": }
@@ -28,15 +28,15 @@ minecraft::whitelist { "ty2tori": }
 minecraft::whitelist { "giobovo0700": }
 minecraft::whitelist { "Garbanzo_Bean": }
 minecraft::whitelist { "WWE_FAN_BRUNO": }
-minecraft::whitelist { "cfujtysdr": }
+minecraft::whitelist { "hitzcreepers": }
 # Define any server properties (e.g. message of the day)
 # desired for the Minecraft server
-minecraft::server_prop { 'motd':
-  value => "Welcome to the Minecraft Server",
-}minecraft::server_prop { 'enable-command-block': value => 'true'}
+minecraft::server_prop { 'motd': value => "Welcome to Byron's Minecraft Server",
+}
+minecraft::server_prop { 'enable-command-block': value => 'true'}
 minecraft::server_prop { 'max-players':value => '9'}
-minecraft::server_prop { 'pvp':
-  value => 'false'}minecraft::server_prop { 'white-list':value => 'true'}
+minecraft::server_prop { 'pvp':  value => 'false'}
+minecraft::server_prop { 'white-list':value => 'true'}
 
 
 # This message of the day file is seen when ssh-ing
